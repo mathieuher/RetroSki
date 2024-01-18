63c27596956008980ba7c5df142f5404697c2745
Mathieu Hermann <mathieu.he@gmail.com>
2024-01-18 20:05:41 +0100

    improve config for specific track style

---
+++ src/models/gates-config.ts
@@ -0,0 +1,9 @@
+export interface GatesConfig {
+    maxWidth: number,
+    minWidth: number,
+    maxHorizontalDistance: number,
+    minVerticalDistance: number,
+    maxVerticalDistance: number,
+    minNumber: number,
+    maxNumber: number
+}
