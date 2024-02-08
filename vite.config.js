e2788ab17513a70ccf04d9399403f038bf018c51
Mathieu Hermann <mathieu.he@gmail.com>
2024-02-08 12:17:47 +0100

    refactor(app): downgrade to 0.28.6 to avoid build or serve error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	base: './',
 	publicDir: 'src/public',
-	build: {
-		rollupOptions: {
-			external: 'excalibur/build/dist/Util/Util',
-		},
-	},
 });
