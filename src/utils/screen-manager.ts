c6491b1c4706e55ff2c544be53818fb87a441ca1
Mathieu Hermann <mathieu.he@gmail.com>
2024-02-13 15:06:43 +0100

    fix(screenManager): quick fix canvas height

--- src/utils/screen-manager.ts
+++ src/utils/screen-manager.ts
@@ -1,11 +1,7 @@
-import { Actor, Camera } from "excalibur";
+import { Actor, Camera } from 'excalibur';
 
 export class ScreenManager {
-
-    public static isNearScreen(item: Actor, camera: Camera): boolean {
-        return (
-            Math.abs(camera.y - item.pos.y) <
-            item.scene.engine.halfCanvasHeight
-        );
-    }
+	public static isNearScreen(item: Actor, camera: Camera): boolean {
+		return Math.abs(camera.y - item.pos.y) < item.scene.engine.canvasHeight;
+	}
 }
