13ae0c14f56d5ac0088490b7f557308795fdc039
Mathieu H. <mathieu.he@gmail.com>
2024-02-15 19:37:28 +0100

    feat(spectators-group): WIP add sound when passing by

--- src/actors/spectator-group.ts
+++ src/actors/spectator-group.ts
@@ -1,50 +1,85 @@
-import { Actor, Color, Engine, Vector, vec } from 'excalibur';
+import { Actor, Engine, Vector, vec, Audio } from 'excalibur';
 import { Config } from '../config';
 import { ScreenManager } from '../utils/screen-manager';
 import { Spectator } from './spectator';
+import { Race } from '../scenes/race';
+import { Game } from '../game';
+import { Resources } from '../resources';
 
 export class SpectatorGroup extends Actor {
-	private engine: Engine;
-	private direction: 'left' | 'right';
-	private density: number;
+    private engine: Engine;
+    private side: 'left' | 'right';
+    private density: number;
+    private sound = Config.SPECTATORS_SOUNDS[Math.round(Math.random() * (Config.SPECTATORS_SOUNDS.length - 1))];
+    private soundInstance?: Audio;
+    private shouldPlayIntenseSound = Math.random() < 0.15;
 
-	constructor(engine: Engine, position: Vector, density: number, direction: 'left' | 'right') {
-		super({
-			anchor: vec(0, 0),
-			pos: position,
-			height: density * Config.SPECTATOR_PRIVATE_SPACE_HEIGHT,
-			width: Config.DISPLAY_MIN_MARGIN,
-		});
+    constructor(engine: Engine, position: Vector, density: number, side: 'left' | 'right') {
+        super({
+            anchor: vec(0, 0),
+            pos: position,
+            height: density * (Config.SPECTATOR_HEIGHT * 0.7),
+            width: Config.DISPLAY_MIN_MARGIN,
+        });
 
-		this.engine = engine;
-		this.density = density;
-		this.direction = direction;
+        this.engine = engine;
+        this.density = density;
+        this.side = side;
 
-		this.listenExitViewportEvent();
-	}
+        this.listenExitViewportEvent();
 
-	update(): void {
-		if (ScreenManager.isNearScreen(this, this.scene.camera) && !this.children?.length) {
-			this.buildSpectators();
-		}
-	}
+    }
 
-	private listenExitViewportEvent(): void {
-		this.on('exitviewport', () => this.checkForKill());
-	}
+    update(): void {
+        if (ScreenManager.isNearScreen(this, this.scene.camera) && !this.children?.length) {
+            this.buildSpectators();
+            (this.engine as Game).soundPlayer.playSound(this.sound, 0.001, true, true);
+            this.soundInstance = this.sound.instances[this.sound.instanceCount() - 1];
+        }
 
-	private checkForKill(): void {
-		if (ScreenManager.isBehind(this.scene.camera.pos, this.pos)) {
-			this.kill();
-		}
-	}
+        if (this.soundInstance) {
+            this.adjustSoundVolume();
+        }
 
-	private buildSpectators(): void {
-		for (let i = 1; i <= this.density; i++) {
-			const xPosition = Math.random() * (this.width - Config.SPECTATOR_PRIVATE_SPACE_WIDTH);
-			const yPosition = i * Config.SPECTATOR_PRIVATE_SPACE_HEIGHT;
-			const rotation = this.direction === 'left' ? 180 : 0;
-			this.addChild(new Spectator(vec(xPosition, yPosition), rotation));
-		}
-	}
+        if (ScreenManager.isBehind(this.scene.camera.pos, this.pos.add(vec(0, this.height))) && this.shouldPlayIntenseSound) {
+            this.shouldPlayIntenseSound = false;
+            (this.engine as Game).soundPlayer.playSound(Resources.SpectatorsIntenseSound, 0.3, false, true);
+        }
+
+
+        if (this.children?.length) {
+            this.rotateSpectators((this.scene as Race).skier!.pos);
+        }
+    }
+
+    private listenExitViewportEvent(): void {
+        this.on('exitviewport', () => this.checkForKill());
+    }
+
+    private checkForKill(): void {
+        if (ScreenManager.isBehind(this.scene.camera.pos, this.pos)) {
+            this.soundInstance?.stop();
+            this.kill();
+        }
+    }
+
+    private buildSpectators(): void {
+        for (let i = 1; i <= this.density; i++) {
+            const xPosition = Math.random() * (this.width - Config.SPECTATOR_WIDTH);
+            const yPosition = Math.random() * (this.height - Config.SPECTATOR_HEIGHT);
+            const rotation = this.side === 'left' ? 0 : 180;
+            this.addChild(new Spectator(vec(xPosition, yPosition), rotation));
+        }
+    }
+
+    private adjustSoundVolume(): void {
+        const skierYPosition = (this.scene as Race).skier!.pos.y;
+        const distance = Math.min(Math.abs(this.pos.y - skierYPosition), 300);
+        this.soundInstance!.volume = Math.max(0.001, 1 - (distance / 300)) * (this.density / 20) * 0.1;
+    }
+
+    private rotateSpectators(skierPosition: Vector): void {
+    }
+
+
 }
--- src/config.ts
+++ src/config.ts
@@ -3,198 +3,202 @@ import { Resources } from './resources';
 import { TrackStyles } from './models/track-styles.enum';
 
 export class Config {
-	// DISPLAY
-	static DISPLAY_WIDTH = 800;
-	static DISPLAY_HEIGHT = 800;
-	static CAMERA_ZOOM = 1;
-	static DISPLAY_MIN_MARGIN = 75;
-	static DISPLAY_MAX_RIGHT_POSITION = Config.DISPLAY_WIDTH / 2 - Config.DISPLAY_MIN_MARGIN;
-	static DISPLAY_MAX_LEFT_POSITION = -Config.DISPLAY_MAX_RIGHT_POSITION;
-	static FRONT_GHOST_DISTANCE = -Config.DISPLAY_HEIGHT / 3.8;
+    // DISPLAY
+    static DISPLAY_WIDTH = 800;
+    static DISPLAY_HEIGHT = 800;
+    static CAMERA_ZOOM = 1;
+    static DISPLAY_MIN_MARGIN = 75;
+    static DISPLAY_MAX_RIGHT_POSITION = Config.DISPLAY_WIDTH / 2 - Config.DISPLAY_MIN_MARGIN;
+    static DISPLAY_MAX_LEFT_POSITION = -Config.DISPLAY_MAX_RIGHT_POSITION;
+    static FRONT_GHOST_DISTANCE = -Config.DISPLAY_HEIGHT / 3.8;
 
-	// SOUND
-	static RACE_AMBIANCE_SOUND_VOLUME = 0.1;
-	static BRAKING_SOUND_VOLUME = 0.3;
-	static CARVING_SOUND_VOLUME = 0.15;
-	static FINISH_SOUND_VOLUME = 0.2;
-	static GATE_MISSED_SOUND_VOLUME = 0.25;
-	static POLE_HIT_SOUND_VOLUME = 0.08;
+    // SOUND
+    static RACE_AMBIANCE_SOUND_VOLUME = 0.1;
+    static BRAKING_SOUND_VOLUME = 0.3;
+    static CARVING_SOUND_VOLUME = 0.15;
+    static FINISH_SOUND_VOLUME = 0.2;
+    static GATE_MISSED_SOUND_VOLUME = 0.25;
+    static POLE_HIT_SOUND_VOLUME = 0.08;
 
-	// RACE
-	static MISSED_GATE_PENALTY_TIME = 3000;
-	static SECTORS_PER_RACE = 2;
-	static SECTOR_DISPLAY_TIME = 4500;
+    // RACE
+    static MISSED_GATE_PENALTY_TIME = 3000;
+    static SECTORS_PER_RACE = 2;
+    static SECTOR_DISPLAY_TIME = 4500;
 
-	// FORMAT
-	static FORMAT_TIMING = 'mm:ss:SS';
+    // FORMAT
+    static FORMAT_TIMING = 'mm:ss:SS';
 
-	// TRACKS
-	static CURRENT_BUILDER_VERSION = 3;
-	static DEFAULT_TRACKS = ['soelden', 'davos', 'wengen', 'adelboden', 'zermatt'];
+    // TRACKS
+    static CURRENT_BUILDER_VERSION = 3;
+    static DEFAULT_TRACKS = ['soelden', 'davos', 'wengen', 'adelboden', 'zermatt'];
 
-	// CONTROLS
-	static KEYBOARD_CONTROL_CARVE_RIGHT = Keys.ArrowRight;
-	static KEYBOARD_CONTROL_CARVE_LEFT = Keys.ArrowLeft;
-	static KEYBOARD_CONTROL_BRAKE = Keys.Space;
-	static KEYBOARD_DEBUG_KEY = Keys.D;
-	static KEYBOARD_RESTART_KEY = Keys.R;
-	static KEYBOARD_EXIT_KEY = Keys.Escape;
-	static KEYBOARD_START_KEY = Keys.ArrowUp;
-	static KEYBOARD_GHOST_KEY = Keys.G;
+    // CONTROLS
+    static KEYBOARD_CONTROL_CARVE_RIGHT = Keys.ArrowRight;
+    static KEYBOARD_CONTROL_CARVE_LEFT = Keys.ArrowLeft;
+    static KEYBOARD_CONTROL_BRAKE = Keys.Space;
+    static KEYBOARD_DEBUG_KEY = Keys.D;
+    static KEYBOARD_RESTART_KEY = Keys.R;
+    static KEYBOARD_EXIT_KEY = Keys.Escape;
+    static KEYBOARD_START_KEY = Keys.ArrowUp;
+    static KEYBOARD_GHOST_KEY = Keys.G;
 
-	static GAMEPAD_AXES_FILTER_RATE = 0.1;
-	static GAMEPAD_CONTROL_CARVE = Axes.LeftStickX;
-	static GAMEPAD_CONTROL_BRAKE = Buttons.Face2;
-	static GAMEPAD_EXIT_BUTTON = Buttons.Select;
-	static GAMEPAD_RACE_BUTTON = Buttons.Face1;
-	static GAMEPAD_START_BUTTON = Buttons.Start;
-	static GAMEPAD_GHOST_BUTTON = Buttons.LeftBumper;
+    static GAMEPAD_AXES_FILTER_RATE = 0.1;
+    static GAMEPAD_CONTROL_CARVE = Axes.LeftStickX;
+    static GAMEPAD_CONTROL_BRAKE = Buttons.Face2;
+    static GAMEPAD_EXIT_BUTTON = Buttons.Select;
+    static GAMEPAD_RACE_BUTTON = Buttons.Face1;
+    static GAMEPAD_START_BUTTON = Buttons.Start;
+    static GAMEPAD_GHOST_BUTTON = Buttons.LeftBumper;
 
-	static TOUCH_BRAKE_ZONE_RATIO = 0.2;
+    static TOUCH_BRAKE_ZONE_RATIO = 0.2;
 
-	// DYNAMIC
-	static MAX_SPEED = 150;
-	static VELOCITY_MULTIPLIER_RATE = 7 * 0.6;
-	static LATERAL_VELOCITY_ROTATION_RATE = 1.08;
-	// ROTATION
-	static MAX_RIGHT_ROTATION_ANGLE = Math.PI / 2;
-	static MAX_LEFT_ROTATION_ANGLE = (3 * Math.PI) / 2;
-	static ROTATION_RECENTER_RATE = 0.2;
+    // DYNAMIC
+    static MAX_SPEED = 150;
+    static VELOCITY_MULTIPLIER_RATE = 7 * 0.6;
+    static LATERAL_VELOCITY_ROTATION_RATE = 1.08;
+    // ROTATION
+    static MAX_RIGHT_ROTATION_ANGLE = Math.PI / 2;
+    static MAX_LEFT_ROTATION_ANGLE = (3 * Math.PI) / 2;
+    static ROTATION_RECENTER_RATE = 0.2;
 
-	// SLOPE
-	static INITIAL_SLOPE = 0.1;
+    // SLOPE
+    static INITIAL_SLOPE = 0.1;
 
-	// SKIER
-	static ACCELERATION_RATE = 3;
-	static BRAKING_RATE = 1;
-	static CARVING_ADHERENCE_RATE = 0.9;
-	static CARVING_BRAKING_RATE = 0.01;
-	static SLIDING_ADHERENCE_RATE = 0.6;
-	static SLIDING_BRAKING_RATE = 0.9;
-	// SKIER SPECIFIC STYLE DYNAMIC
-	static SL_SKIER_CONFIG = {
-		windFrictionRate: 0.0025,
-		carvingRotationRate: 3.2,
-		carvingOptimalSpeed: 50,
-		slidingRotationRate: 4.2,
-		slidingOptimalSpeed: 35,
-	};
-	static GS_SKIER_CONFIG = {
-		windFrictionRate: 0.0022,
-		carvingRotationRate: 3.1,
-		carvingOptimalSpeed: 60,
-		slidingRotationRate: 4.1,
-		slidingOptimalSpeed: 45,
-	};
-	static SG_SKIER_CONFIG = {
-		windFrictionRate: 0.002,
-		carvingRotationRate: 2.6,
-		carvingOptimalSpeed: 70,
-		slidingRotationRate: 3.2,
-		slidingOptimalSpeed: 50,
-	};
-	static DH_SKIER_CONFIG = {
-		windFrictionRate: 0.0018,
-		carvingRotationRate: 2.2,
-		carvingOptimalSpeed: 75,
-		slidingRotationRate: 3.1,
-		slidingOptimalSpeed: 60,
-	};
+    // SKIER
+    static ACCELERATION_RATE = 3;
+    static BRAKING_RATE = 1;
+    static CARVING_ADHERENCE_RATE = 0.9;
+    static CARVING_BRAKING_RATE = 0.01;
+    static SLIDING_ADHERENCE_RATE = 0.6;
+    static SLIDING_BRAKING_RATE = 0.9;
+    // SKIER SPECIFIC STYLE DYNAMIC
+    static SL_SKIER_CONFIG = {
+        windFrictionRate: 0.0025,
+        carvingRotationRate: 3.2,
+        carvingOptimalSpeed: 50,
+        slidingRotationRate: 4.2,
+        slidingOptimalSpeed: 35,
+    };
+    static GS_SKIER_CONFIG = {
+        windFrictionRate: 0.0022,
+        carvingRotationRate: 3.1,
+        carvingOptimalSpeed: 60,
+        slidingRotationRate: 4.1,
+        slidingOptimalSpeed: 45,
+    };
+    static SG_SKIER_CONFIG = {
+        windFrictionRate: 0.002,
+        carvingRotationRate: 2.6,
+        carvingOptimalSpeed: 70,
+        slidingRotationRate: 3.2,
+        slidingOptimalSpeed: 50,
+    };
+    static DH_SKIER_CONFIG = {
+        windFrictionRate: 0.0018,
+        carvingRotationRate: 2.2,
+        carvingOptimalSpeed: 75,
+        slidingRotationRate: 3.1,
+        slidingOptimalSpeed: 60,
+    };
 
-	// GATES
-	static GATE_MAX_LEFT_POSITION = Config.DISPLAY_MAX_LEFT_POSITION;
-	static GATE_MAX_RIGHT_POSITION = Config.DISPLAY_MAX_RIGHT_POSITION;
-	static FINAL_GATE_WIDTH = Config.DISPLAY_WIDTH - 2 * Config.DISPLAY_MIN_MARGIN;
-	static FINAL_GATE_POSITION = Config.DISPLAY_MAX_LEFT_POSITION;
-	static GATE_OTHER_SIDE_PROBABILITY = 0.95;
-	// SPECIFIC TRACK STYLE CONFIG
-	static SL_GATES_CONFIG = {
-		trackStyle: TrackStyles.SL,
-		maxWidth: 114,
-		minWidth: 90,
-		maxHorizontalDistance: 192,
-		minVerticalDistance: 102,
-		maxVerticalDistance: 162,
-		minNumber: 41,
-		maxNumber: 51,
-		poleWidth: 3,
-		poleHeight: 20,
-		poleSprites: new Map([
-			['red', Resources.PoleSlRed.toSprite()],
-			['blue', Resources.PoleSlBlue.toSprite()],
-		]),
-		poleCheckSprites: new Map([
-			['red', Resources.PoleCheckRed.toSprite()],
-			['blue', Resources.PoleCheckBlue.toSprite()],
-		]),
-	};
-	static GS_GATES_CONFIG = {
-		trackStyle: TrackStyles.GS,
-		maxWidth: 120,
-		minWidth: 96,
-		maxHorizontalDistance: 192,
-		minVerticalDistance: 120,
-		maxVerticalDistance: 210,
-		minNumber: 44,
-		maxNumber: 54,
-		poleWidth: 12,
-		poleHeight: 12,
-		poleSprites: new Map([
-			['red', Resources.PoleRed.toSprite()],
-			['blue', Resources.PoleBlue.toSprite()],
-		]),
-		poleCheckSprites: new Map([
-			['red', Resources.PoleCheckRed.toSprite()],
-			['blue', Resources.PoleCheckBlue.toSprite()],
-		]),
-	};
-	static SG_GATES_CONFIG = {
-		trackStyle: TrackStyles.SG,
-		maxWidth: 144,
-		minWidth: 114,
-		maxHorizontalDistance: 210,
-		minVerticalDistance: 192,
-		maxVerticalDistance: 300,
-		minNumber: 46,
-		maxNumber: 56,
-		poleWidth: 12,
-		poleHeight: 12,
-		poleSprites: new Map([
-			['red', Resources.PoleRed.toSprite()],
-			['blue', Resources.PoleBlue.toSprite()],
-		]),
-		poleCheckSprites: new Map([
-			['red', Resources.PoleCheckRed.toSprite()],
-			['blue', Resources.PoleCheckBlue.toSprite()],
-		]),
-	};
-	static DH_GATES_CONFIG = {
-		trackStyle: TrackStyles.DH,
-		maxWidth: 150,
-		minWidth: 120,
-		maxHorizontalDistance: 180,
-		minVerticalDistance: 252,
-		maxVerticalDistance: 318,
-		minNumber: 48,
-		maxNumber: 58,
-		poleWidth: 12,
-		poleHeight: 12,
-		poleSprites: new Map([['red', Resources.PoleRed.toSprite()]]),
-		poleCheckSprites: new Map([['red', Resources.PoleCheckRed.toSprite()]]),
-	};
+    // GATES
+    static GATE_MAX_LEFT_POSITION = Config.DISPLAY_MAX_LEFT_POSITION;
+    static GATE_MAX_RIGHT_POSITION = Config.DISPLAY_MAX_RIGHT_POSITION;
+    static FINAL_GATE_WIDTH = Config.DISPLAY_WIDTH - 2 * Config.DISPLAY_MIN_MARGIN;
+    static FINAL_GATE_POSITION = Config.DISPLAY_MAX_LEFT_POSITION;
+    static GATE_OTHER_SIDE_PROBABILITY = 0.95;
+    // SPECIFIC TRACK STYLE CONFIG
+    static SL_GATES_CONFIG = {
+        trackStyle: TrackStyles.SL,
+        maxWidth: 114,
+        minWidth: 90,
+        maxHorizontalDistance: 192,
+        minVerticalDistance: 102,
+        maxVerticalDistance: 162,
+        minNumber: 41,
+        maxNumber: 51,
+        poleWidth: 3,
+        poleHeight: 20,
+        poleSprites: new Map([
+            ['red', Resources.PoleSlRed.toSprite()],
+            ['blue', Resources.PoleSlBlue.toSprite()],
+        ]),
+        poleCheckSprites: new Map([
+            ['red', Resources.PoleCheckRed.toSprite()],
+            ['blue', Resources.PoleCheckBlue.toSprite()],
+        ]),
+    };
+    static GS_GATES_CONFIG = {
+        trackStyle: TrackStyles.GS,
+        maxWidth: 120,
+        minWidth: 96,
+        maxHorizontalDistance: 192,
+        minVerticalDistance: 120,
+        maxVerticalDistance: 210,
+        minNumber: 44,
+        maxNumber: 54,
+        poleWidth: 12,
+        poleHeight: 12,
+        poleSprites: new Map([
+            ['red', Resources.PoleRed.toSprite()],
+            ['blue', Resources.PoleBlue.toSprite()],
+        ]),
+        poleCheckSprites: new Map([
+            ['red', Resources.PoleCheckRed.toSprite()],
+            ['blue', Resources.PoleCheckBlue.toSprite()],
+        ]),
+    };
+    static SG_GATES_CONFIG = {
+        trackStyle: TrackStyles.SG,
+        maxWidth: 144,
+        minWidth: 114,
+        maxHorizontalDistance: 210,
+        minVerticalDistance: 192,
+        maxVerticalDistance: 300,
+        minNumber: 46,
+        maxNumber: 56,
+        poleWidth: 12,
+        poleHeight: 12,
+        poleSprites: new Map([
+            ['red', Resources.PoleRed.toSprite()],
+            ['blue', Resources.PoleBlue.toSprite()],
+        ]),
+        poleCheckSprites: new Map([
+            ['red', Resources.PoleCheckRed.toSprite()],
+            ['blue', Resources.PoleCheckBlue.toSprite()],
+        ]),
+    };
+    static DH_GATES_CONFIG = {
+        trackStyle: TrackStyles.DH,
+        maxWidth: 150,
+        minWidth: 120,
+        maxHorizontalDistance: 180,
+        minVerticalDistance: 252,
+        maxVerticalDistance: 318,
+        minNumber: 48,
+        maxNumber: 58,
+        poleWidth: 12,
+        poleHeight: 12,
+        poleSprites: new Map([['red', Resources.PoleRed.toSprite()]]),
+        poleCheckSprites: new Map([['red', Resources.PoleCheckRed.toSprite()]]),
+    };
 
-	// POLES
-	static POLE_DETECTOR_MARGIN = 18;
-	static FINAL_POLE_WIDTH = 30;
-	static FINAL_POLE_HEIGHT = 60;
+    // POLES
+    static POLE_DETECTOR_MARGIN = 18;
+    static FINAL_POLE_WIDTH = 30;
+    static FINAL_POLE_HEIGHT = 60;
 
-	// SPECTATORS
-	static SPECTATOR_PRIVATE_SPACE_HEIGHT = 18;
-	static SPECTATOR_PRIVATE_SPACE_WIDTH = 15;
-	static SPECTATOR_SPRITES = [
-		Resources.Spectator1.toSprite(),
-		Resources.Spectator2.toSprite(),
-		Resources.Spectator3.toSprite(),
-		Resources.Spectator4.toSprite(),
-	];
+    // SPECTATORS
+    static SPECTATOR_HEIGHT = 18;
+    static SPECTATOR_WIDTH = 15;
+    static SPECTATOR_SPRITES = [
+        Resources.Spectator1.toSprite(),
+        Resources.Spectator2.toSprite(),
+        Resources.Spectator3.toSprite(),
+        Resources.Spectator4.toSprite(),
+    ];
+    static SPECTATORS_SOUNDS = [
+        Resources.SpectatorsSound,
+        Resources.Spectators2Sound
+    ];
 }
