e4a039f04f2e42edf1a7206e85e95083ae039338
Mathieu Hermann <mathieu.he@gmail.com>
2024-02-15 10:11:24 +0100

    fix(touchManager): fix touch zone detection on reduced screen

--- src/utils/touch-manager.ts
+++ src/utils/touch-manager.ts
@@ -1,50 +1,47 @@
-import { Engine, Vector } from "excalibur";
-import { Config } from "../config";
+import { Engine, Vector } from 'excalibur';
+import { Config } from '../config';
 
 export class TouchManager {
-    private engine: Engine;
+	private engine: Engine;
 
-    public isTouching = false;
-    public isTouchingBack = false;
-    public isTouchingLeft = false;
-    public isTouchingRight = false;
+	public isTouching = false;
+	public isTouchingBack = false;
+	public isTouchingLeft = false;
+	public isTouchingRight = false;
 
-    constructor(engine: Engine) {
-        this.engine = engine;
-        this.listenTouch();
-    }
+	constructor(engine: Engine) {
+		this.engine = engine;
+		this.listenTouch();
+	}
 
-    private listenTouch(): void {
-        this.engine.input.pointers.on('down', (event) => {
-            this.recomputeTouchStatus('down', event.screenPos);
-        });
+	private listenTouch(): void {
+		this.engine.input.pointers.on('down', event => {
+			this.recomputeTouchStatus('down', event.pagePos);
+		});
 
-        this.engine.input.pointers.on('up', (event) => {
-            this.recomputeTouchStatus('up', event.screenPos);
-        });
-    }
+		this.engine.input.pointers.on('up', event => {
+			this.recomputeTouchStatus('up', event.pagePos);
+		});
+	}
 
-    private recomputeTouchStatus(type: 'down' | 'up', position: Vector): void {
-        if (this.getTouchZone(position) === 'back') {
-            this.isTouchingBack = type === 'down';
-        } else if (this.getTouchZone(position) === 'left') {
-            this.isTouchingLeft = type === 'down';
-        } else if (this.getTouchZone(position) === 'right') {
-            this.isTouchingRight = type === 'down';
-        }
-        this.isTouching = this.isTouchingBack || this.isTouchingLeft || this.isTouchingRight;
-    }
+	private recomputeTouchStatus(type: 'down' | 'up', position: Vector): void {
+		if (this.getTouchZone(position) === 'back') {
+			this.isTouchingBack = type === 'down';
+		} else if (this.getTouchZone(position) === 'left') {
+			this.isTouchingLeft = type === 'down';
+		} else if (this.getTouchZone(position) === 'right') {
+			this.isTouchingRight = type === 'down';
+		}
+		this.isTouching = this.isTouchingBack || this.isTouchingLeft || this.isTouchingRight;
+	}
 
-    private getTouchZone(position: Vector): 'back' | 'left' | 'right' {
-        if (position.y > this.engine.drawHeight - (Config.TOUCH_BRAKE_ZONE_RATIO * this.engine.drawHeight)) {
-            return 'back';
-        }
-        if (position.x > Config.DISPLAY_WIDTH / 2) {
-            return 'right';
-        }
-        return 'left';
-    }
-
-
-
+	private getTouchZone(position: Vector): 'back' | 'left' | 'right' {
+		if (position.y > window.innerHeight - Config.TOUCH_BRAKE_ZONE_RATIO * window.innerHeight) {
+			return 'back';
+		}
+		if (position.x > window.innerWidth / 2) {
+			return 'right';
+		}
+		return 'left';
+	}
 }
