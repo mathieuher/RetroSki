3329ee56a64600bfb7563c049bf63386715367ac
Mathieu Hermann <mathieu.hermann@hug.ch>
2024-11-02 01:38:28 +0100

    feat(learning): WIP implements documentation

--- src/app/pages/learning/learning.component.ts
+++ src/app/pages/learning/learning.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, model } from '@angular/core';
 import { ButtonIconComponent } from '../../common/components/button-icon/button-icon.component';
 import { ToolbarComponent } from '../../common/components/toolbar/toolbar.component';
+import { RouterLink } from '@angular/router';
+import { StepperComponent } from '../../common/components/stepper/stepper.component';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-learning',
   standalone: true,
-  imports: [ButtonIconComponent, ToolbarComponent],
+  imports: [ButtonIconComponent, NgTemplateOutlet,  RouterLink, StepperComponent, ToolbarComponent],
   templateUrl: './learning.component.html',
   styleUrl: './learning.component.scss'
 })
 export class LearningComponent {
+    protected readonly totalPage = 4;
+    protected currentPage = model(1);
+    protected currentPageName = computed(() => `page_${this.currentPage()}`);
+
 
 }
