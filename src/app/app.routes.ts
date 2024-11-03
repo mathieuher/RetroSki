2421e7bc4940757ee7172e2d5c7773f654467965
mathieuher <mathieu.he@gmail.com>
2024-11-03 11:31:18 +0100

    feat(settings): add settings on angular app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LearningComponent } from './pages/learning/learning.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'learning', component: LearningComponent },
+    { path: 'settings', component: SettingsComponent },
     { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
