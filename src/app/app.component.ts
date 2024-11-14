af72b5c58e95f8c8bedbbcc46980f7ba5ea0ff2c
Mathieu Hermann <mathieu.hermann@hug.ch>
2024-11-14 15:54:28 +0100

    fix(screen): improve responsive for landscape and add update text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,4 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    title = 'RetroSki';
 }
