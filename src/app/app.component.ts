5d304523e9011151c8f52156d01f7ebd791c67f8
mathieuher <mathieu.he@gmail.com>
2024-11-14 16:25:39 +0100

    feat(app): update design to inform user of smaller screen about compatible device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component, Signal, signal } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationComponent } from "./common/components/notification/notification.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NotificationComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+    protected screenCompatible: WritableSignal<boolean>;
+
+    constructor() {
+        this.screenCompatible = signal(screen.height >= 500);
+
+        addEventListener('resize', () => this.screenCompatible.set(screen.height >= 500));
+    }
 }
