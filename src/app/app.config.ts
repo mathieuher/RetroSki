8c4f8ba995bd9e54c47b4103e0734e774be0c233
Mathieu Hermann <mathieu.he@gmail.com>
2024-12-18 22:41:06 +0100

    feat(service-worker): add service-worker + loading placeholder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { type ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
+    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 };
