e98b80929a328c67a49f049131207afd3e91a73e
Mathieu Hermann <mathieu.he@gmail.com>
2025-01-05 11:32:43 +0100

    refact(app): fix formating

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,12 @@ import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideServiceWorker('ngsw-worker.js', {
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes, withHashLocation()),
+        provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+        })
+    ]
 };
